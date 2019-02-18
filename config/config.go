2d378ef0c543e2977f4581cf08a7090eb6c724dd
Julien SENON <julien.senon@gmail.com>
2019-02-18 22:32:05 +0100

    Fix linter

--- config/config.go
+++ config/config.go
@@ -7,6 +7,11 @@ const (
 )
 
 // Dynamic version retrieve with ldflags
+// Version represent version of application
 var Version string
+
+// GitCommit represent git commit
 var GitCommit string
+
+// BuildDate represent date of build
 var BuildDate string
